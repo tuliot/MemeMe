9110d62f409858daadc4fc74ee58e777efb5330c
Tulio Troncoso <tuliot@live.com>
2016-08-22 20:09:17 -0400

    Added referencing outlets to editor ui elements

--- MemeMe/MemeMe/EditorViewController.swift
+++ MemeMe/MemeMe/EditorViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class EditorViewController: UIViewController {
 
+    // MARK: Editor Properties
+    
+    @IBOutlet weak var topToolbar: UIToolbar!
+    @IBOutlet weak var bottomToolbar: UIToolbar!
+
+    @IBOutlet weak var topTextField: UITextField!
+
+    @IBOutlet weak var bottomTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
